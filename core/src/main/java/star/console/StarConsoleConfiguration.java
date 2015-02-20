b46f6d33b5b8af910edad3ad9291e43c1adcc3c9
maprohu <maprohu@gmail.com>
2015-02-20 16:50:44 +0000

    bean starter

--- core/src/main/java/star/console/StarConsoleConfiguration.java
+++ core/src/main/java/star/console/StarConsoleConfiguration.java
@@ -2,13 +2,12 @@ package star.console;
 
 import io.hawt.embedded.Main;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 
 import star.app.StarAppConfiguration;
 
@@ -19,30 +18,24 @@ import com.google.common.base.Throwables;
 @Import(StarAppConfiguration.class)
 public class StarConsoleConfiguration {
 	
-	@Component
-	public static class HawtioStarter implements CommandLineRunner {
-		
-		@Autowired
-		ServerProperties serverProperties;
-		
-		@Autowired
-		StarConsoleProperties starConsoleProperties;
-
-		@Override
-		public void run(String... args) throws Exception {
-			try {
-				System.setProperty("hawtio.offline", "true");
-				System.setProperty("hawtio.dirname", "/tmp/hawtio");
-				System.setProperty("hawtio.authenticationEnabled", Boolean.toString(starConsoleProperties.isAuthenticationEnabled()));
-				Main main = new Main();
-				main.setPort(serverProperties.getPort());
-				main.setWar("res/hawtio-web.war");
-				main.run(false);
-			} catch (Exception e) {
-				throw Throwables.propagate(e);
+	@Bean
+	public CommandLineRunner hawtioStarter(final ServerProperties serverProperties, final StarConsoleProperties starConsoleProperties) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				try {
+					System.setProperty("hawtio.offline", "true");
+					System.setProperty("hawtio.dirname", "/tmp/hawtio");
+					System.setProperty("hawtio.authenticationEnabled", Boolean.toString(starConsoleProperties.isAuthenticationEnabled()));
+					Main main = new Main();
+					main.setPort(serverProperties.getPort());
+					main.setWar("res/hawtio-web.war");
+					main.run(false);
+				} catch (Exception e) {
+					throw Throwables.propagate(e);
+				}
 			}
-		}
+		};
 	}
-	
 
 }
